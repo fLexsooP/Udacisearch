2fa3fa304eb30cec2159a47b17e2d634fa99751e
fLexsooP <ascc594557067@gmail.com>
2022-09-12 00:51:10 -0700

    final commit

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -43,6 +43,11 @@ public final class WebCrawlerMain {
       resultWriter.write(new OutputStreamWriter(System.out));
     }
     // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+    if (!config.getResultPath().isEmpty()) {
+      resultWriter.write(Path.of(config.getProfileOutputPath()));
+    } else {
+      resultWriter.write(new OutputStreamWriter(System.out));
+    }
   }
 
   public static void main(String[] args) throws Exception {
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -2,8 +2,14 @@ package com.udacity.webcrawler.profiler;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.ZonedDateTime;
 import java.util.Objects;
@@ -33,13 +39,31 @@ final class ProfilerImpl implements Profiler {
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    boolean hasAnnotatedMethod = false;
+    for(Method method:klass.getMethods()){
+      if(method.getAnnotation(Profiled.class) != null){
+        hasAnnotatedMethod = true;
+        break;
+      }
+    }
+    if(!hasAnnotatedMethod){
+      throw new IllegalArgumentException("Class does not have any methods with @Profiles annotation");
+    }
+    return (T)Proxy.newProxyInstance(klass.getClassLoader(),
+            new Class[]{klass},
+            new ProfilingMethodInterceptor(clock, delegate, state));
+//    return delegate;
   }
 
   @Override
   public void writeData(Path path) {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    try (Writer writer = new OutputStreamWriter(Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+      writeData(writer);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
   }
 
   @Override
